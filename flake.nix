36d0a7b7b10c7fa3c0b6776a34eaebfcbe65deb5
Ingo Ruhnke <grumbel@gmail.com>
2021-11-09 16:38:58 +0100

    Fix dependency duplication in nix flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nix, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -16,7 +18,10 @@
             pname = "tinycmmc";
             version = "0.1.0";
             src = nixpkgs.lib.cleanSource ./.;
-            nativeBuildInputs = [ pkgs.cmake pkgs.ninja ];
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.ninja
+            ];
           };
         };
         defaultPackage = packages.tinycmmc;
